bd7f02d179a4bd82c09c5f442d7867f2b0a8c71e
gaga-ziyuan <53992532+gaga-ziyuan@users.noreply.github.com>
2019-08-11 19:05:32 +1000

    Add files via upload

---
+++ js/includes.js
@@ -0,0 +1,11 @@
+document.write(
+    '<link rel="shortcut icon" href="../images/favicon.ico">' +
+    '<link rel="stylesheet" href="../styles/wiki-assets.css">' +
+    '<script src="../js/header.js" async></script>' +
+    '<script src="../js/sidebar.js" async></script>' +
+    '<script src="../js/folder.js" async></script>' +
+    '<script src="../js/footnotes.js" async></script>' +
+    '<script src="../js/wiki-walk.js" async></script>' +
+    '<script src="../js/random-page.js" async></script>' +
+    '<script src="../js/window-onload.js" async></script>'
+);
---
+++ js/wiki-walk.js
@@ -0,0 +1,26 @@
+var page_num = 7;
+
+function addZero(num, addStr = '', appendStr = '') {
+    if (num < 1 || num > page_num) {
+        if (addStr === 'scp-yc-') return '#';
+        return '无';
+    }
+    let str = num.toString();
+    while (str.length < 3) {
+        str = '0' + str;
+    }
+    return addStr + str + appendStr;
+}
+
+function initWikiWalk() {
+    let title = document.head.getElementsByTagName('title')[0];
+    let page = parseInt(title.innerHTML.slice(7, 10));
+    if (isNaN(page)) return;
+    let mainContent = document.getElementById('main-content');
+    let walk = document.createElement('div');
+    mainContent.appendChild(walk);
+    walk.outerHTML = '' +
+    '<div class="wiki-walk">' +
+    '   « <a href="' + addZero(page-1, 'scp-yc-', '.html') + '">' + addZero(page-1, 'SCP-YC-') + '</a> | SCP-YC-' + addZero(page) + ' | <a href="' + addZero(page+1, 'scp-yc-', '.html') + '">' + addZero(page+1, 'SCP-YC-') + '</a> »' +
+    '</div>';
+}
